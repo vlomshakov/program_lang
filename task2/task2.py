8400b716aaa92e40ceb0b6fe5360f64b5a200311
vlomshakov <vadim.lomshakov@gmail.com>
2013-02-22 02:52:06 +0400

    ver2

--- task2/task2.py
+++ task2/task2.py
@@ -6,11 +6,11 @@ import sys
 
 # helper classes
 class Holder(object):
-  def set(self, value):
-    self.value = value
-    return value
-  def get(self):
-    return self.value
+	def set(self, value):
+		self.value = value
+		return value
+	def get(self):
+		return self.value
 
 class SingletonMeta(type):
 	def __init__(cls, name, bases, dict):
@@ -21,6 +21,18 @@ class SingletonMeta(type):
 			self.instance = super(SingletonMeta, self).__call__(*args, **kw)
 		return self.instance
 #---------------------------------------------------------------------------
+class SyntaxError_(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+	def __str__(self):
+		return self.msg
+
+
+class RunTimeError(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+	def __str__(self):
+		return self.msg
 
 
 class Context(object):
@@ -28,7 +40,7 @@ class Context(object):
 	A context is singleton. This class keeps state of computation system.
 	"""
 	__metaclass__ = SingletonMeta
-  
+
 	_vars = {}
 	def readInputStream(self):
 		return int(raw_input(""))
@@ -43,53 +55,11 @@ class Context(object):
 		try:
 			return self._vars[name]
 		except KeyError:
-			raise RunTimeError("[erorr][Interpretater] used undefined variable: "+name)
+			raise RunTimeError("[error][interpreterLangL] used undefined variable: "+name)
 
 
-
-
-#postorder
-class NodeIterator(object):
-	def __init__(self, node):
-		self.stack = [[node,False]]
-
-	def __iter__(self):
-		return self
-
-	def next(self):
-		if not self.stack:
-			raise StopIteration
-		
-		node = self.stack[-1][0]
-		visit = self.stack[-1][1]
-		while node.getLeft() and not visit:
-			self.stack[-1][1] = True
-			if node.getMid(): self.stack.append([node.getMid(),False])
-			if node.getRight():	self.stack.append([node.getRight(),False])
-			self.stack.append([node.getLeft(),False])
-			node = node.getLeft()
-		
-		return self.stack.pop()[0]
 #-----------------------------------------------------------------------------------
-class Node(object):
-	_left = None;
-	_right = None;
-	_mid   = None;
-	
-	def getLeft(self): return self._left
-	def getRight(self): return self._right
-	def getMid(self): return self._mid
-
-	def preVisit(self):
-		raise NotImplementedError('Method Node.travels is pure virtual')
-
-	def postVisit(self):
-		raise NotImplementedError('Method Node.travels is pure virtual')
-
-	def __iter__(self):
-		return NodeIterator(self)
-#-----------------------------------------------------------------------------------
-class Expr(Node):
+class Expr(object):
 	def evaluate(self):
 		raise NotImplementedError('Method Expr.evaluate is pure virtual')
 
@@ -112,15 +82,15 @@ class Number(Expr):
 
 class BinOperator(Expr):
 	def __init__(self, left, right, op):
-		Parent._left = left
-		Parent._right = right
+		self._left = left
+		self._right = right
 		self._op = op
 	
 	def evaluate(self):
-		return int (eval(str(self.getLeft().evaluate()) + self._op + str(self.getRight().evaluate()) ) )
+		return int (eval(str(self._left.evaluate()) + self._op + str(self._right.evaluate()) ) )
 
 #-----------------------------------------------------------------------------------
-class Stmt(Node):
+class Stmt(object):
 	def interpret(self):
 		raise NotImplementedError('Method Stmt.interpret is pure virtual')
 
@@ -130,63 +100,156 @@ class SkipStmt(Stmt):
 		pass
 	
 class AssignStmt(Stmt):
-	def __init__(self, left, right):
-		Parent._left = left
-		Parent._right = right
+	def __init__(self, var, expr):
+		self._var = var
+		self._expr = expr
 	def interpret(self):
-		Context().assigNewValue(self.getLeft().getName(), self.getRight().evaluate())
+		Context().assigNewValue(self._var.getName(), self._expr.evaluate())
 
+	
 class ReadStmt(Stmt):
-	def __init__(self, left):
-		Parent._left = left
+	def __init__(self, var):
+		self._var = var
 	def interpret(self):
-		Context().assigNewValue(self.getLeft().getName(), Context().readInputStream())		
+		Context().assigNewValue(self._var.getName(), Context().readInputStream())
+
 	
 class WriteStmt(Stmt):
-	def __init__(self, left):
-		Parent._left = left
+	def __init__(self, expr):
+		self._expr = expr
 	def interpret(self):
-		Context().writeOutputStream(self.getLeft().evaluate())
+		Context().writeOutputStream(self._expr.evaluate())
 
+	
 class SequenceStmt(Stmt):
-	def __init__(self, left, right):
-		Parent._left = left
-		Parent._right = right
+	def __init__(self, stmt1, stmt2):
+		self._stmt1 = stmt1
+		self._stmt2 = stmt2
 	def interpret(self):
-		self.getLeft().interpret()
-		self.getLeft().interpret()
+		self._stmt1.interpret()
+		self._stmt2.interpret()
 
+	
 class IfStmt(Stmt):
-	def __init__(self, cond, stmt1, stmt2):
-		Parent._left = cond
-		Parent._mid = stmt1
-		Parent._right = stmt2
+	def __init__(self, expr, stmt1, stmt2):
+		self._expr = expr
+		self._stmt1 = stmt1
+		self._stmt2 = stmt2
 	def interpret(self):
-		cond = self.getLeft().evaluate()
-		if cond == 0:
-			self.getRight().interpret()
+		cond = self._expr.evaluate()
+		if cond != 0:
+			self._stmt1.interpret()
 		else:
-			self.getMid().interpret()
+			self._stmt2.interpret()
 
+	
 class WhileStmt(Stmt):
-	def __init__(self, cond, stmt):
-		Parent._left = cond
-		Parent._right = stmt
+	def __init__(self, expr, stmt):
+		self._expr = expr
+		self._stmt = stmt
 	def interpret(self):
 		cond = Holder()
-		while cond.set(self.getLeft().evaluate()):
-			self.getRight().interpret()
+		while cond.set(self._expr.evaluate()):
+			self._stmt.interpret()
+#---------------------------------------------------------------------------
+
+class InterpreterLangL(object):
+	"""
+Interpreter language "L" implemented based semantic rule "big step"
+	"""
+	def __init__(self, instream):
+		self._in = instream
+		self._scopeInterp = True
+
+	def _get_line(self):
+		return self._in.readline().strip()
+
+	def interpretTree(self):
+		"""
+Visits postorder of tree during reading file and interprets just in time.
+Statement shrink after interpreting.
+		"""
+		symbol = self._get_line()
+		node = None
+		if not symbol:
+			return
+
+		if symbol == '!':
+			node = Number(int(self._get_line()))
+		elif symbol == 'x':
+			node = Variable(self._get_line())
+		elif symbol == '@':
+			op = self._get_line()
+			expr1 = self.interpretTree()
+			expr2 = self.interpretTree()
+			node = BinOperator(expr1, expr2, op)
+		elif symbol == 's':
+			node = SkipStmt()
+		elif symbol == '=':
+			var = Variable(self._get_line()) 
+			expr = self.interpretTree()
+			node = AssignStmt(var, expr)
+		elif symbol == 'w':
+			expr = self.interpretTree()
+			node = WriteStmt(expr)
+		elif symbol == 'r':
+			var = Variable(self._get_line())
+			node = ReadStmt(var)
+		elif symbol == ';':
+			stmt1 = self.interpretTree()
+			stmt2 = self.interpretTree()	
+			node = SequenceStmt(stmt1, stmt2)
+		elif symbol == 'i':
+			outScopeInterp = self._scopeInterp
+			self._scopeInterp = False
+			expr  = self.interpretTree()
+			stmt1 = self.interpretTree()
+			stmt2 = self.interpretTree()
+			self._scopeInterp = outScopeInterp
+			node = IfStmt(expr, stmt1, stmt2)
+		elif symbol == 'l':
+			outScopeInterp = self._scopeInterp
+			self._scopeInterp = False
+			expr  = self.interpretTree()
+			stmt = self.interpretTree()
+			self._scopeInterp = outScopeInterp
+			node = WhileStmt(expr, stmt)
+		else:
+			raise SyntaxError_("[error][interpreterLangL]not recognize the symbol:\n"+repr(symbol))
+
+		if isinstance(node, Stmt) and self._scopeInterp and \
+		not isinstance(node, SequenceStmt):
+			node.interpret()
+
+# shrink tree after interpreting
+		if isinstance(node, Stmt) and self._scopeInterp:
+			return SkipStmt()
+		else: 
+			return node
+		
+			
+		
+		
 		
 
 
 
 
+if len(sys.argv) != 2:
+	print "no input file"
+	sys.exit(1)
+
+f = None
+try:
+	f = open(sys.argv[1],"r")
+	InterpreterLangL(f).interpretTree()
+except IOError as e:
+	print "I/O error({0}): {1}".format(e.errno, e.strerror)
+except SyntaxError_ as e:
+	print e
+except RunTimeError as e:
+	print e
+finally:
+	if f: f.close()
 
 
-
-
-
-r = Node(Node(None,None), Node(Node(None,None),None))
-
-for i in r:
-	print i.val
